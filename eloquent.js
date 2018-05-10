446555ad9908aaf96be329fd21c15b812e9a0d6e
dannash100 <danster15@gmail.com>
2018-05-11 10:56:57 +1200

    exercise 3a

--- eloquent.js
+++ eloquent.js
@@ -1,4 +1,4 @@
-//Exercise:1
+//Exercise 1:
 
 function min(num1, num2) {
     return Math.min(num1, num2);
@@ -7,7 +7,7 @@ function min(num1, num2) {
 console.log(min(21, 32));
 
 
-//Exercise:2
+//Exercise 2:
 
 function isEven(num) {
     if (num === 1) {
@@ -18,3 +18,18 @@ function isEven(num) {
 }
 
 console.log(isEven(-1));
+
+//Exercise 3:
+
+function countBs(str){
+    var count = 0
+    for (i=0; i < str.length; i++) {
+        if (str[i] === 'B') {
+            count ++;
+        } 
+    } return count;
+}
+
+
+console.log(countBs("BBC"));
+
