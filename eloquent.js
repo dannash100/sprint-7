1423d8daf54235f58ebceac2dab572ad8687eaa9
dannash100 <danster15@gmail.com>
2018-05-10 19:43:22 +1200

    exercise 2 complete

---
+++ eloquent.js
@@ -0,0 +1,22 @@
+//Exercise:1
+
+function min(num1, num2) {
+    return Math.min(num1, num2);
+}
+
+console.log(min(21, 32));
+
+
+//Exercise:2
+
+function isEven(num) {
+    if (num = 1) {
+        return false;
+    } else if (num % 2 === num - 2 % 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isEven(-1));
